fa4169848af06a53a299044fd4c2e2613750bf46
agavshin <alexey.gavshin@just.ru.com>
2018-02-03 22:13:15 +0300

    [JavaEE]: reimplementation

--- spring-rabbitmq/src/main/java/ru/agavshin/service/DemoService.java
+++ spring-rabbitmq/src/main/java/ru/agavshin/service/DemoService.java
@@ -1,5 +1,6 @@
 package ru.agavshin.service;
 
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -7,18 +8,27 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import ru.agavshin.entity.Demo;
+
+import static org.springframework.util.Assert.*;
 
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @Service
 @EnableBinding(Processor.class)
 public class DemoService {
 
-    @Autowired
-    private Logger logger;
+    private final Logger logger;
+
+    private final Processor processor;
 
     @Autowired
-    private Processor processor;
+    public DemoService(Logger logger, Processor processor) {
+        notNull(logger, "Error logger injecting.");
+        this.logger = logger;
+        notNull(processor, "Error processor injecting.");
+        this.processor = processor;
+    }
 
     public Demo sample() {
         return Demo.createSample();
@@ -26,11 +36,13 @@ public class DemoService {
 
     public void send(Demo demo) {
         logger.info("Demo has been sent. {}", demo);
-        processor.output().send(MessageBuilder.withPayload(demo).build());
+        String json = new Gson().toJson(demo);
+        processor.output().send(MessageBuilder.withPayload(json.getBytes()).build());
     }
 
     @StreamListener(Processor.INPUT)
-    public void receive(Demo demo) {
+    public void receive(String json) {
+        Demo demo = new Gson().fromJson(json, Demo.class);
         logger.info("Demo has been received. {}", demo);
     }
 }
